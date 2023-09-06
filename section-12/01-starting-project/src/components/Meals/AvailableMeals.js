a7a070f5aa92ee087b2758a61c7522deee351dc6
an.vu <an.vu@bnksolution.com>
2023-09-06 16:40:28 +0700

    finish lesson 178 forwardRef

--- section-12/01-starting-project/src/components/Meals/AvailableMeals.js
+++ section-12/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
@@ -28,17 +30,25 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => {
-    return <li>{meal.name}</li>
+  const mealsList = DUMMY_MEALS.map((meal) => {
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      ></MealItem>
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>
-        {mealsList}
-      </ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-  )
+  );
 };
 
 export default AvailableMeals;
